6590bc32bb9fa47299355469998e9728139c28d4
Ton Martín <tonmg2006@gmail.com>
2025-05-05 19:36:08 +0200

    first commit

--- src/prog2/model/BombaRefrigerant.java
+++ src/prog2/model/BombaRefrigerant.java
@@ -5,12 +5,13 @@ public class BombaRefrigerant implements InBombaRefrigerant{
     private int id;
     private boolean activat;
     private boolean foraDeServei;
+    private VariableUniforme variableUniforme;
+    private float capacitat;
+    private float costOperatiu;
 
-
-    public BombaRefrigerant (int id, boolean activat, boolean foraDeServei) {
+    public BombaRefrigerant (int id, VariableUniforme variableUniforme) {
         this.id = id;
-        this.activat = activat;
-        this.foraDeServei = foraDeServei;
+        this.variableUniforme = variableUniforme;
     }
 
     @Override
@@ -24,28 +25,42 @@ public class BombaRefrigerant implements InBombaRefrigerant{
     @Override
     public boolean getForaDeServei() { return foraDeServei; }
 
+    public void setForaDeServei(boolean foraDeServei) { this.foraDeServei = foraDeServei; }
+
     @Override
     public float getCapacitat() {
-        return 0;
+        if (getActivat()) {
+            return capacitat;
+        } else { return 0;}
     }
 
     @Override
     public float getCostOperatiu() {
-        return 0;
+        if (getActivat()) {
+            return costOperatiu;
+        } else { return 0;}
     }
 
     @Override
     public void activa() throws CentralUBException {
-
+        setActivat(true);
     }
 
     @Override
     public void desactiva() {
-
+        setActivat(false);
     }
 
     @Override
     public void revisa(PaginaIncidencies p) {
+        int numAleatori = variableUniforme.seguentValor();
+        if (numAleatori > 75) {
+            this.setForaDeServei(true);
+        }
+    }
 
+    public String toString(){
+        return "Id = " + getId() + ", Activat = " + getActivat() +
+                ", ForaDeServei = " + getForaDeServei();
     }
 }
