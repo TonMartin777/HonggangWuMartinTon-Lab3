a7946ca2210aa3f0a956537ae1ae1418a1696681
Ton Martín <tonmg2006@gmail.com>
2025-05-06 12:24:46 +0200

    first commit

--- src/prog2/model/PaginaEstat.java
+++ src/prog2/model/PaginaEstat.java
@@ -1,7 +1,29 @@
 package prog2.model;
 
 public class PaginaEstat extends PaginaBitacola {
-    public PaginaEstat(int dia) {
+    private int grauInsercioBarres;
+    private String outputReactor;
+    private String outputSistemaRefrigeracio;
+    private String outputGeneradorVapor;
+    private String outputTurbina;
+
+    public PaginaEstat(int dia, int grauInsercioBarres, String outputReactor,
+                       String outputSistemaRefrigeracio, String outputGeneradorVapor, String outputTurbina) {
         super(dia);
+        this.grauInsercioBarres = grauInsercioBarres;
+        this.outputReactor = outputReactor;
+        this.outputSistemaRefrigeracio = outputSistemaRefrigeracio;
+        this.outputGeneradorVapor = outputGeneradorVapor;
+        this.outputTurbina = outputTurbina;
+    }
+
+    @Override
+    public String toString() {
+        return "- Dia: " + getDia() + "\n"
+                + "- Grau Insercio: " + grauInsercioBarres + "\n"
+                + "- Output Reactor: " + outputReactor + "\n"
+                + "- Output Sistema Refrigeració: " + outputSistemaRefrigeracio + "\n"
+                + "- Output Generador de Vapor: " + outputGeneradorVapor + "\n"
+                + "- Output Turbina: " + outputTurbina + "\n";
     }
 }
